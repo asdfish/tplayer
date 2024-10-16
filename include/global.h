94ba7dcb052274caefe67f5fa2e2df0a0b692c84
asdfish <lgray3420@gmail.com>
2024-10-16 07:43:28 -0400

    removed unused c++ compiler

--- include/global.h
+++ include/global.h
@@ -1,7 +1,7 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
-#define PLAYLISTS_DIRECTORY "/home/andre/files/music"
+#define PLAYLISTS_DIRECTORY "/path/to/your/playlists"
 
 extern const char* menu_selection_prefix;
 
