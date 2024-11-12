c16547035587850130066ee26938a25826ecfd8c
asdfish <lgray3420@gmail.com>
2024-11-12 06:25:49 -0500

    made private functions static

---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#ifdef CONFIG_INCLUDE_PLAYLISTS_PATH
+#ifndef CONFIG_INCLUDED_PLAYLISTS_PATH
+#define CONFIG_INCLUDED_PLAYLISTS_PATH
+static const char* playlists_path = "/path/to/your/playlists";
+#endif
+#endif
+
+#endif
