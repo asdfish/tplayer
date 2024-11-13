803de85d61046e6859b63cf70db6e5b0956289d2
asdfish <lgray3420@gmail.com>
2024-11-13 11:43:24 -0500

    added initing menu items

--- src/tplayer.c
+++ src/tplayer.c
@@ -1,10 +1,15 @@
+#define CONFIG_INCLUDE_MENU_CONFIGS
 #define CONFIG_INCLUDE_PLAYLISTS_PATH
 #include <config.h>
 #include <filesystem.h>
 
+#include <tb_menu.h>
+
 #include <stdlib.h>
 
 #define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+static int array_to_menu_items(const char** array, unsigned int array_length, struct TbMenuItem** items);
 
 static void free_playlist_names(void);
 static void free_playlist_names_contents(void);
@@ -15,15 +20,51 @@ static void free_playlists_contents_until(unsigned int until);
 
 static void free_playlists_lengths(void);
 
+static void free_playlist_menu_items(void);
+static void free_playlists_menus_items(void);
+static void free_playlists_menus_items_contents(void);
+static void free_playlists_menus_items_contents_until(unsigned int until);
+
 static void free_all(void);
+static void free_all_playlists(void);
+static void free_all_menu_items(void);
 
 static int init(void);
+static int init_menu_items(void);
+static int init_playlists(void);
 
 const char** playlist_names = NULL;
 unsigned int playlist_names_length = 0;
 
 const char*** playlists = NULL;
 unsigned int* playlists_lengths = NULL;
+
+unsigned int selected_playlist = 0;
+
+struct TbMenu playlist_menu;
+struct TbMenu* playlists_menus = NULL;
+
+struct TbMenuItem* playlist_menu_items = NULL;
+struct TbMenuItem** playlists_menus_items = NULL;
+
+static int array_to_menu_items(const char** array, unsigned int array_length, struct TbMenuItem** items) {
+  if(array == NULL || items == NULL)
+    return EXIT_FAILURE;
+  if(array_length == 0)
+    return EXIT_SUCCESS;
+
+  *items = (struct TbMenuItem*) malloc(array_length * sizeof(struct TbMenuItem));
+  if(*items == NULL)
+    return EXIT_FAILURE;
+
+  for(unsigned int i = 0; i < array_length; i ++) {
+    (*items)[i].foreground = menu_foreground;
+    (*items)[i].foreground_reversed = menu_foreground_reversed;
+    (*items)[i].contents = array[i];
+  }
+
+  return EXIT_SUCCESS;
+}
 
 static void free_playlist_names(void) {
   free(playlist_names);
@@ -59,7 +100,29 @@ static void free_playlists_lengths(void) {
   playlists_lengths = NULL;
 }
 
+static void free_playlist_menu_items(void) {
+  free(playlist_menu_items);
+  playlist_menu_items = NULL;
+}
+static void free_playlists_menus_items(void) {
+  free(playlists_menus_items);
+  playlists_menus_items = NULL;
+}
+static void free_playlists_menus_items_contents(void) {
+  free_playlists_menus_items_contents_until(playlist_names_length);
+}
+static void free_playlists_menus_items_contents_until(unsigned int until) {
+  for(unsigned int i = 0; i < until; i ++) {
+    free(playlists_menus_items[i]);
+    playlists_menus_items[i] = NULL;
+  }
+}
+
 static void free_all(void) {
+  free_all_playlists();
+  free_all_menu_items();
+}
+static void free_all_playlists(void) {
   free_playlists_contents();
   free_playlists();
 
@@ -68,8 +131,53 @@ static void free_all(void) {
 
   free_playlists_lengths();
 }
+static void free_all_menu_items(void) {
+  free_playlists_menus_items_contents();
+  free_playlists_menus_items();
+
+  free_playlist_menu_items();
+}
 
 static int init(void) {
+  if(init_playlists() != EXIT_SUCCESS)
+    return EXIT_FAILURE;
+  if(init_menu_items() != EXIT_SUCCESS)
+    goto free_all_playlists;
+  return EXIT_SUCCESS;
+
+free_all_playlists:
+  free_all_playlists();
+  return EXIT_FAILURE;
+}
+static int init_menu_items(void) {
+  if(playlist_names == NULL || playlist_names_length == 0 || playlists == NULL || playlists_lengths == NULL)
+    return EXIT_FAILURE;
+
+  if(array_to_menu_items(playlist_names, playlist_names_length, &playlist_menu_items) != EXIT_SUCCESS)
+    return EXIT_FAILURE;
+
+  playlists_menus_items = (struct TbMenuItem**) malloc(playlist_names_length * sizeof(struct TbMenuItem*));
+  if(playlists_menus_items == NULL)
+    goto free_playlist_menu_items;
+
+  unsigned int i = 0;
+  while(i < playlist_names_length) {
+    if(array_to_menu_items(playlists[i], playlists_lengths[i], playlists_menus_items + i) != EXIT_SUCCESS)
+      goto free_menus_items_until;
+    i ++;
+  }
+
+  return EXIT_SUCCESS;
+
+free_menus_items_until:
+  free_playlists_menus_items_contents_until(i);
+  free_playlists_menus_items();
+
+free_playlist_menu_items:
+  free_playlist_menu_items();
+  return EXIT_FAILURE;
+}
+static int init_playlists(void) {
   if(get_dirents_info(playlists_path, DIRENT_IS_DIRECTORY, DIRENT_PATH, &playlist_names, &playlist_names_length) != EXIT_SUCCESS)
     return EXIT_FAILURE;
 
@@ -103,7 +211,7 @@ free_playlist_names:
 int tplayer(void) {
   if(init() != EXIT_SUCCESS)
     return EXIT_FAILURE;
+
   free_all();
   return EXIT_SUCCESS;
 }
-
