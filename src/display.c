8c5babcac69e063e202fa93f3daa8ff21783adf4
asdfish <lgray3420@gmail.com>
2024-11-14 10:26:34 -0500

    made menus render properly

--- src/display.c
+++ src/display.c
@@ -11,6 +11,11 @@ int display_draw(void) {
     playlists_menus + selected_playlist
   };
 
+  tb_printf(69, 0, 0, 0, "x %u", playlists_menus[0].x);
+  tb_printf(69, 1, 0, 0, "y %u", playlists_menus[0].y);
+  tb_printf(69, 2, 0, 0, "x %u", playlists_menus[0].width);
+  tb_printf(69, 3, 0, 0, "y %u", playlists_menus[0].height);
+
   for(unsigned int i = 0; i < 2; i ++) {
     if(i == selected_menu)
       menus[i]->enable_reversed_colors = true;
@@ -34,10 +39,13 @@ int display_resize(void) {
   unsigned int playlist_menu_width = terminal_width * menu_split;
   unsigned int playlists_menus_width = terminal_width - playlist_menu_width;
 
+  playlist_menu.x = 0;
+  playlist_menu.y = 0;
   playlist_menu.width = playlist_menu_width;
   playlist_menu.height = terminal_height;
   for(unsigned int i = 0; i < playlist_names_length; i ++) {
     playlists_menus[i].x = playlist_menu_width;
+    playlists_menus[i].y = 0;
     playlists_menus[i].width = playlists_menus_width;
     playlists_menus[i].height = terminal_height;
   }
